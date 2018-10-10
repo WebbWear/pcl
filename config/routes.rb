7dede931ad99f96b566d752e03eddd3ebd226e95
Jeff Webb <webbwear@icloud.com>
2018-10-10 16:09:11 -0400

    Catch all

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get "log/:driver_id/:id", to: "driver_log#edit", as: "driver_log"
   patch "log/:driver_id/:id", to: "driver_log#update"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "*path", to: "application#fallback_index_html", constraints: -> (request) do
+                 !request.xhr? && request.format.html?
+               end
 end
